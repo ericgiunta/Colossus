a8a9a101fb0ccaccd9c2b149e385c9b1813df79d
User <egiunta@ksu.edu>
2023-07-25 13:42:26 -0500

    added bash script to configure the makevar script

---
+++ Colossus/configure_MAKEVAR.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#make the Makevars file
+if [ ! -e "./src/Makevars" ]; then
+touch ./src/Makevars
+fi
+
+
+case "$OSTYPE" in
+  linux*)   echo 'PKG_CXXFLAGS=-fopenmp PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) -fopenmp' > ./src/Makevars ;;
+  darwin*)  echo "PKG_CPPFLAGS='-Xclang -fopenmp' PKG_LIBS=-lomp R CMD INSTALL Colossus'" > ./src/Makevars ;; 
+  msys*)    echo "windows" ;;
+  solaris*) echo "solaris" ;;
+  bsd*)     echo "bsd" ;;
+  *)        echo "unknown" ;;
+esac
+
