562f75d7cebbe4e4dc3bb1fd673dd28d16be0866
ericgiunta <69258239+ericgiunta@users.noreply.github.com>
2023-07-26 10:33:43 -0500

    Update configure_MAKEVAR.sh

--- Colossus/configure_MAKEVAR.sh
+++ Colossus/configure_MAKEVAR.sh
@@ -10,6 +10,7 @@ case "$OSTYPE" in
   linux*)   echo 'PKG_CXXFLAGS=-fopenmp
 PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) -fopenmp' > ./src/Makevars ;;
   darwin*)  echo 'PKG_CPPFLAGS='-Xclang -fopenmp'
+LDFLAGS="-L/usr/local/opt/llvm/lib/c++ -Wl,-rpath,/usr/local/opt/llvm/lib/c++"
 LDFLAGS += -L/opt/homebrew/opt/libomp/lib -lomp
 CPPFLAGS += -I/opt/homebrew/opt/libomp/include -Xclang -fopenmp' > ./src/Makevars ;; 
   msys*)    echo "windows" ;;
