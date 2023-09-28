04e08b149546e98f281486ad2c636de8dcc1d208
ericgiunta <69258239+ericgiunta@users.noreply.github.com>
2023-09-28 14:55:04 -0500

    Update OS configure

--- Colossus/tools/config/configure.R
+++ Colossus/tools/config/configure.R
@@ -2,18 +2,36 @@
 # Use 'define()' to define configuration variables.
 # Use 'configure_file()' to substitute configuration values.
 
-if (Sys.info()['sysname']=="Linux"){
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
+os <- get_os()
+
+if (os=="linux"){
     define(PKG_CXXFLAGS = "PKG_CXXFLAGS=-fopenmp")
     define(PKG_LIBS = 'PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) -fopenmp')
     define(CPPFLAGS = "#CPPFLAGS += -Xclang -fopenmp")
     define(LDFLAGS = "#LDFLAGS += -lomp")
-} else if (Sys.info()['sysname']=="Darwin"){
+} else if (os=="osx"){
     define(PKG_CXXFLAGS = "#PKG_CXXFLAGS=-fopenmp")
     define(PKG_LIBS = '#PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) -fopenmp')
     define(CPPFLAGS = "CPPFLAGS += -Xclang -fopenmp")
     define(LDFLAGS = "LDFLAGS += -lomp")
 } else {
-    print(paste("OS",Sys.info()['sysname'],sep=" "))
+    print(paste("OS",os,sep=" "))
     define(PKG_CXXFLAGS = "PKG_CXXFLAGS=-fopenmp")
     define(PKG_LIBS = '#PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) -fopenmp')
     define(CPPFLAGS = "#CPPFLAGS += -Xclang -fopenmp")
