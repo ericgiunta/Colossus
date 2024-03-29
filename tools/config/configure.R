5722a643eafc6beb8b88e6f91daff6bf6c60ab1f
ericgiunta <egiunta@ksu.edu>
2024-03-29 12:43:55 -0500

    Updated MacOS configuration and removed intentional failure workflow

--- tools/config/configure.R
+++ tools/config/configure.R
@@ -51,45 +51,10 @@ Rcomp_version <- function() {
   out
 }
 
-Rcpp_version <- function() {
-  out <- tryCatch(processx::run("head", "~/.R/Makevars", stderr_to_stdout = TRUE),
-                  error = function(cnd) list(stdout = ""))
-  out0 <- stringr::str_match(out$stdout, "clang")[1]
-  if (!is.na(out0)){
-  	out <- "clang"
-  } else {
-    out0 <- stringr::str_match(out$stdout, "gcc")[1]
-    if (!is.na(out0)){
-      out <- "gcc"
-    } else {
-      out <- out$stdout
-    }
-  }
-  out
-}
-
 os <- get_os()
 cpp_compiler <- gcc_version()
 R_compiler <- Rcomp_version()
-R_Make_Comp <- Rcpp_version()
 
-#if (os=="linux"){
-#
-#    define(PKG_CXXFLAGS = "PKG_CXXFLAGS= $(SHLIB_OPENMP_CXXFLAGS)")
-#    define(PKG_LIBS = 'PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) $(SHLIB_OPENMP_CXXFLAGS)')
-#    define(PKG_CPPFLAGS = "#PKG_CPPFLAGS = -Xclang -fopenmp")
-#    #configure_file("src/Makevars.in")
-#} else if (os=="osx"){
-#    define(PKG_CXXFLAGS = "PKG_CXXFLAGS= $(SHLIB_OPENMP_CXXFLAGS)")
-#    define(PKG_LIBS = '#LDFLAGS = -lomp"')
-#    define(PKG_CPPFLAGS = "#PKG_CPPFLAGS = -Xclang -fopenmp")
-#    #configure_file("src/Makevars.in")
-#} else {
-#    define(PKG_CXXFLAGS = "PKG_CXXFLAGS= $(SHLIB_OPENMP_CXXFLAGS)")
-#    define(PKG_LIBS = 'PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) $(SHLIB_OPENMP_CXXFLAGS)')
-#    define(PKG_CPPFLAGS = "#PKG_CPPFLAGS = -Xclang -fopenmp")
-#    #configure_file("src/Makevars.win.in")
-#}
 
 if (os=="linux"){
 	if (cpp_compiler=="gcc"){
@@ -98,32 +63,32 @@ if (os=="linux"){
             print("CONFIG NOTE: Building linux with gcc and openmp support")
 		    define(PKG_CXXFLAGS = "PKG_CXXFLAGS = $(SHLIB_OPENMP_CXXFLAGS)")
 		    define(PKG_LIBS = 'PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) $(SHLIB_OPENMP_CXXFLAGS)')
-		    define(PKG_CPPFLAGS = "#PKG_CPPFLAGS += -Xclang $(SHLIB_OPENMP_CXXFLAGS)")
-		    define(LDFLAGS = "#LDFLAGS += -lomp")
+		    define(PKG_CPPFLAGS = "#PKG_CPPFLAGS")
+		    define(LDFLAGS = "#LDFLAGS")
 		    configure_file("src/Makevars.in")
 	    } else {
             print("CONFIG NOTE: Identified linux system with c++ default to gcc and R compiled with clang")
             print("CONFIG NOTE: Building linux with clang and no openmp support")
-	        define(PKG_CXXFLAGS = "#PKG_CXXFLAGS = $(SHLIB_OPENMP_CXXFLAGS)")
+	        define(PKG_CXXFLAGS = "#PKG_CXXFLAGS")
 		    define(PKG_LIBS = 'PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)')
-		    define(PKG_CPPFLAGS = "#PKG_CPPFLAGS += -Xclang $(SHLIB_OPENMP_CXXFLAGS)")
-		    define(LDFLAGS = "#LDFLAGS += -lomp")
+		    define(PKG_CPPFLAGS = "#PKG_CPPFLAGS")
+		    define(LDFLAGS = "#LDFLAGS")
 		    configure_file("src/Makevars.in")
 	    }
 	} else if (cpp_compiler=='clang'){
         print("CONFIG NOTE: Identified linux system with c++ default to clang")
         print("CONFIG NOTE: Building linux with clang and no openmp support")
-		define(PKG_CXXFLAGS = "#PKG_CXXFLAGS= $(SHLIB_OPENMP_CXXFLAGS)")
+		define(PKG_CXXFLAGS = "#PKG_CXXFLAGS")
 		define(PKG_LIBS = 'PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)')
-		define(PKG_CPPFLAGS = "#PKG_CPPFLAGS += -Xclang $(SHLIB_OPENMP_CXXFLAGS)")
-		define(LDFLAGS = "#LDFLAGS += -lomp")
+		define(PKG_CPPFLAGS = "#PKG_CPPFLAGS")
+		define(LDFLAGS = "#LDFLAGS")
 		configure_file("src/Makevars.in")
 	}
 } else if (os=="osx"){
     print("CONFIG NOTE: Building mac and allowing xclang openmp support")
-    define(PKG_CXXFLAGS = "#PKG_CXXFLAGS= $(SHLIB_OPENMP_CXXFLAGS)")
-    define(PKG_LIBS = '#PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) $(SHLIB_OPENMP_CXXFLAGS)')
-    define(PKG_CPPFLAGS = "PKG_CPPFLAGS += -Xclang $(SHLIB_OPENMP_CXXFLAGS)")
+    define(PKG_CXXFLAGS = "#PKG_CXXFLAGS")
+    define(PKG_LIBS = '#PKG_LIBS')
+    define(PKG_CPPFLAGS = "PKG_CPPFLAGS = -Xclang -fopenmp")
     define(LDFLAGS = "LDFLAGS += -lomp")
     configure_file("src/Makevars.in")
 } else {
