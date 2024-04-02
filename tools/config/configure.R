6eae92cd105c93cbfcb6414c6df6494d705b9e98
ericgiunta <egiunta@ksu.edu>
2024-04-02 12:36:28 -0500

    Corrected MacOS Makevar file

--- tools/config/configure.R
+++ tools/config/configure.R
@@ -88,7 +88,7 @@ if (os=="linux"){
     print("CONFIG NOTE: Building mac and allowing xclang openmp support")
     define(PKG_CXXFLAGS = "#PKG_CXXFLAGS=-fopenmp")
     define(PKG_LIBS = '#PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) -fopenmp')
-    define(CPPFLAGS = "CPPFLAGS += -Xclang -fopenmp")
+    define(PKG_CPPFLAGS = "CPPFLAGS += -Xclang -fopenmp")
     define(LDFLAGS = "LDFLAGS += -lomp")
     configure_file("src/Makevars.in")
 } else {
