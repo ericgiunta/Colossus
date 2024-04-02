a8efa371af50451ae396b968988d88f9ff603819
ericgiunta <egiunta@ksu.edu>
2024-04-02 12:04:41 -0500

    Reverted MacOS instructions

--- tools/config/configure.R
+++ tools/config/configure.R
@@ -86,9 +86,9 @@ if (os=="linux"){
 	}
 } else if (os=="osx"){
     print("CONFIG NOTE: Building mac and allowing xclang openmp support")
-    define(PKG_CXXFLAGS = "#PKG_CXXFLAGS")
-    define(PKG_LIBS = '#PKG_LIBS')
-    define(PKG_CPPFLAGS = "PKG_CPPFLAGS = -Xclang -fopenmp")
+    define(PKG_CXXFLAGS = "#PKG_CXXFLAGS=-fopenmp")
+    define(PKG_LIBS = '#PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"` $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS) -fopenmp')
+    define(CPPFLAGS = "CPPFLAGS += -Xclang -fopenmp")
     define(LDFLAGS = "LDFLAGS += -lomp")
     configure_file("src/Makevars.in")
 } else {
